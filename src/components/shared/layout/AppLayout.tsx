a4a1f2718def27fd8fae96bc523ddc80f128d3c1
Agustín Longoni <agustin.longoni@protofire.io>
2024-09-25 21:01:07 -0300

    Font + Polymesh Logo + Background (#5)
    * fix poppins  font
    
    * add polymesh logo
    
    * add background
    
    * Trigger CI
    
    ---------
    
    Co-authored-by: Henry Palacios <henrydpalacios@gmail.com>

--- src/components/shared/layout/AppLayout.tsx
+++ src/components/shared/layout/AppLayout.tsx
@@ -1,28 +1,48 @@
 'use client';
 
-import { AppBar, Container, styled, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Container,
+  styled,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import Image from 'next/image';
+import PolymeshLogo from 'public/polymesh-logo.svg';
+import PolymeshBG from 'public/background.png';
 
 const MainContainer = styled(Container)(() => ({
-  minHeight: '100vh',
-  background: 'linear-gradient(to right, #0d0d0d, #1a1a1a)',
   color: '#fff',
 }));
 
 const CustomAppBar = styled(AppBar)({
-  backgroundColor: '#1a1a1a',
+  backgroundColor: 'transparent',
   boxShadow: 'none',
   padding: '1rem 0',
 });
 
 export function AppLayout({ children }: React.PropsWithChildren) {
   return (
-    <>
+    <Box
+      sx={{
+        backgroundImage: `url(${PolymeshBG.src})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundAttachment: 'fixed',
+        minHeight: '100vh',
+        width: '100%',
+      }}
+    >
       <CustomAppBar position="static">
         <Toolbar>
-          <Typography variant="h6">Polymesh Explorer</Typography>
+          <Image src={PolymeshLogo} height={22} alt="Polymesh logo" />
+          <Typography variant="h6" ml={2}>
+            EXPLORER
+          </Typography>
         </Toolbar>
       </CustomAppBar>
       <MainContainer maxWidth="lg">{children}</MainContainer>
-    </>
+    </Box>
   );
 }
