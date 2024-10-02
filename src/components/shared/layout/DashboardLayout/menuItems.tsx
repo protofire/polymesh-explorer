173f318e9da2b5d019a0574db17e01ce788ad886
Agustín Longoni <agustinlongoni@gmail.com>
2024-10-02 11:27:35 -0300

    change icons on menu (#15)
    * change icons on menu
    
    * change venue icon
    
    * Test deploy
    
    ---------
    
    Co-authored-by: Henry Palacios <henrydpalacios@gmail.com>

--- src/components/shared/layout/DashboardLayout/menuItems.tsx
+++ src/components/shared/layout/DashboardLayout/menuItems.tsx
@@ -1,25 +1,28 @@
-import DescriptionIcon from '@mui/icons-material/Description';
+import HomeIcon from '@mui/icons-material/Home';
+import TollIcon from '@mui/icons-material/Toll';
+import RecentActorsIcon from '@mui/icons-material/RecentActors';
+import WhereToVoteIcon from '@mui/icons-material/WhereToVote';
 import { ROUTES } from '@/config/routes';
 
 export const MENU_NAVIGATION = [
   {
     segment: ROUTES.Home,
     title: 'Home',
-    icon: <DescriptionIcon />,
+    icon: <HomeIcon />,
   },
   {
     segment: ROUTES.Identity.substring(1),
     title: 'Identities',
-    icon: <DescriptionIcon />,
+    icon: <RecentActorsIcon />,
   },
   {
     segment: ROUTES.Asset.substring(1),
     title: 'Assets',
-    icon: <DescriptionIcon />,
+    icon: <TollIcon />,
   },
   {
     segment: ROUTES.Venue.substring(1),
     title: 'Venue',
-    icon: <DescriptionIcon />,
+    icon: <WhereToVoteIcon />,
   },
 ];
