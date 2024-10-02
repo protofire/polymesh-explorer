0b0d3c140cfdba3be2e6821de5a278a8288277c6
Agustín Longoni <agustinlongoni@gmail.com>
2024-10-02 11:18:33 -0300

    change venue icon

--- src/components/shared/layout/DashboardLayout/menuItems.tsx
+++ src/components/shared/layout/DashboardLayout/menuItems.tsx
@@ -1,7 +1,7 @@
-import DescriptionIcon from '@mui/icons-material/Description';
 import HomeIcon from '@mui/icons-material/Home';
 import TollIcon from '@mui/icons-material/Toll';
 import RecentActorsIcon from '@mui/icons-material/RecentActors';
+import WhereToVoteIcon from '@mui/icons-material/WhereToVote';
 import { ROUTES } from '@/config/routes';
 
 export const MENU_NAVIGATION = [
@@ -23,6 +23,6 @@ export const MENU_NAVIGATION = [
   {
     segment: ROUTES.Venue.substring(1),
     title: 'Venue',
-    icon: <DescriptionIcon />,
+    icon: <WhereToVoteIcon />,
   },
 ];
