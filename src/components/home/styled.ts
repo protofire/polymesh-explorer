59cfc4d590f032c4028b39f22094a0455b0ca72f
Agustín Longoni <agustinlongoni@gmail.com>
2024-10-03 21:41:17 -0300

    fix Home on mobile

--- src/components/home/styled.ts
+++ src/components/home/styled.ts
@@ -1,10 +1,17 @@
 import { Box, styled } from '@mui/material';
 
-export const CustomBox = styled(Box)({
+export const CustomBox = styled(Box)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   textAlign: 'center',
-  padding: '2rem',
+  padding: theme.spacing(0),
+  [theme.breakpoints.up('md')]: {
+    padding: theme.spacing(4),
+  },
   color: '#fff',
-});
+  marginTop: theme.spacing(8),
+  [theme.breakpoints.up('md')]: {
+    marginTop: theme.spacing(12),
+  },
+}));
