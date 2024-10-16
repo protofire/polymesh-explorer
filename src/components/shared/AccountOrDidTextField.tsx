5061b3a8a5b6879b2521cffa7d0039c03a152e40
Henry Palacios <henrydpalacios@gmail.com>
2024-10-16 10:20:54 -0300

    Use Copybutton component

--- src/components/shared/AccountOrDidTextField.tsx
+++ src/components/shared/AccountOrDidTextField.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Box, Typography, IconButton } from '@mui/material';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { Box, Typography } from '@mui/material';
 import { truncateAddress } from '@/services/polymesh/address';
 import { GenericLink } from './common/GenericLink';
+import CopyButton from './common/CopyButton';
 
 interface AccountOrDidTextFieldProps {
   value: string;
@@ -11,22 +11,19 @@ interface AccountOrDidTextFieldProps {
 export function AccountOrDidTextField({
   value,
 }: AccountOrDidTextFieldProps): React.ReactElement {
-  const handleCopy = () => {
-    navigator.clipboard.writeText(value);
-  };
-
   return (
-    <Box display="flex" alignItems="center" justifyContent="space-between">
+    <Box
+      display="flex"
+      gap={1}
+      alignItems="center"
+      justifyContent="space-between"
+    >
       <Typography variant="body1">
         <GenericLink href={`/account/${value}`}>
           {truncateAddress(value)}
         </GenericLink>
       </Typography>
-      <Box>
-        <IconButton size="small" onClick={handleCopy}>
-          <ContentCopyIcon fontSize="small" />
-        </IconButton>
-      </Box>
+      <CopyButton text={value} />
     </Box>
   );
 }
