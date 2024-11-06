76d4056f619214e00aa133b32e8a038e71e29434
Henry Palacios <henrydpalacios@gmail.com>
2024-11-06 12:11:09 -0300

    Using dash

--- src/components/asset/details/AssetDetailsTabs/AssetTransactionsTab.tsx
+++ src/components/asset/details/AssetDetailsTabs/AssetTransactionsTab.tsx
@@ -225,76 +225,77 @@ export function AssetTransactionsTab({
   }
 
   return (
-    <Stack spacing={3} sx={{ p: 2 }}>
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Typography variant="h6">Asset Transactions</Typography>
-        {uniqueVenues.length > 0 && (
-          <Box display="flex" alignItems="center">
-            <FormControl size="small" sx={{ minWidth: 200 }}>
-              <InputLabel>Filter by Venue</InputLabel>
-              <Select
-                value={selectedVenue}
-                onChange={(e) => {
-                  setSelectedVenue(e.target.value);
-                  setPage(0);
-                }}
-                label="Filter by Venue"
-              >
-                <MenuItem value="all">All Venues</MenuItem>
-                {uniqueVenues.map((venue) => (
-                  <MenuItem key={venue} value={venue}>
-                    Venue {venue}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <Tooltip title="Filtering applies only to the records shown in the table, not all blockchain transactions.">
-              <IconButton size="small">
-                <InfoIcon fontSize="small" />
-              </IconButton>
-            </Tooltip>
-          </Box>
+    <>
+      <Stack spacing={3} sx={{ p: 2 }}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="h6">Asset Transactions</Typography>
+          {uniqueVenues.length > 0 && (
+            <Box display="flex" alignItems="center">
+              <FormControl size="small" sx={{ minWidth: 200 }}>
+                <InputLabel>Filter by Venue</InputLabel>
+                <Select
+                  value={selectedVenue}
+                  onChange={(e) => {
+                    setSelectedVenue(e.target.value);
+                    setPage(0);
+                  }}
+                  label="Filter by Venue"
+                >
+                  <MenuItem value="all">All Venues</MenuItem>
+                  {uniqueVenues.map((venue) => (
+                    <MenuItem key={venue} value={venue}>
+                      Venue {venue}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <Tooltip title="Filtering applies only to the records shown in the table, not all blockchain transactions.">
+                <IconButton size="small">
+                  <InfoIcon fontSize="small" />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          )}
+        </Box>
+
+        {venueFiltering && (
+          <VenueFilteringStatus
+            enabled={venueFiltering.isEnabled}
+            allowedVenues={venueFiltering.allowedVenues}
+          />
         )}
-      </Box>
-
-      {venueFiltering && (
-        <VenueFilteringStatus
-          enabled={venueFiltering.isEnabled}
-          allowedVenues={venueFiltering.allowedVenues}
-        />
-      )}
-
-      <Paper elevation={2}>
-        <TableContainer>
-          <Table>
-            <TableHead>
+      </Stack>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Instruction</TableCell>
+              <TableCell>Venue</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>From</TableCell>
+              <TableCell>To</TableCell>
+              <TableCell align="right">Amount</TableCell>
+              <TableCell align="center">Details</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredTransactions.length === 0 ? (
               <TableRow>
-                <TableCell>Instruction</TableCell>
-                <TableCell>Venue</TableCell>
-                <TableCell>Date</TableCell>
-                <TableCell>Type</TableCell>
-                <TableCell>From</TableCell>
-                <TableCell>To</TableCell>
-                <TableCell align="right">Amount</TableCell>
-                <TableCell align="center">Details</TableCell>
+                <TableCell colSpan={7}>
+                  <TransactionsEmptyState />
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredTransactions.length === 0 ? (
-                <TableRow>
-                  <TableCell colSpan={7}>
-                    <TransactionsEmptyState />
-                  </TableCell>
-                </TableRow>
-              ) : (
-                filteredTransactions.map((tx) => {
-                  const eventInfo = getEventLabel(tx.eventId as EventIdEnum);
-                  const fromDid = tx.fromId?.split('/')[0] || '';
-                  const toDid = tx.toId?.split('/')[0] || '';
+            ) : (
+              filteredTransactions.map((tx) => {
+                const eventInfo = getEventLabel(tx.eventId as EventIdEnum);
+                const fromDid = tx.fromId?.split('/')[0] || '';
+                const toDid = tx.toId?.split('/')[0] || '';
 
-                  return (
-                    <TableRow key={tx.id} hover>
-                      <TableCell>
+                return (
+                  <TableRow key={tx.id} hover>
+                    <TableCell>
+                      {tx.instructionId ? (
                         <GenericLink
                           href={`${subscanUrl}/block/${tx.createdBlock.blockId}?tab=event&event=${tx.id.replace('/', '-')}`}
                           tooltipText="See on subscan"
@@ -302,71 +303,73 @@ export function AssetTransactionsTab({
                         >
                           {tx.instructionId}
                         </GenericLink>
-                      </TableCell>
-                      <TableCell>
-                        {tx.venueId ? (
-                          <GenericLink href={`${ROUTES.Venue}/${tx.venueId}`}>
-                            {tx.venueId}
-                          </GenericLink>
-                        ) : (
-                          <EmptyDash />
-                        )}
-                      </TableCell>
-                      <TableCell>
-                        <FormattedDate date={tx.createdBlock.datetime} />
-                      </TableCell>
-                      <TableCell>
-                        <Chip
-                          label={eventInfo.label}
-                          color={eventInfo.color}
-                          size="small"
-                        />
-                      </TableCell>
-                      <TableCell>
-                        {fromDid ? (
-                          <AccountOrDidTextField value={fromDid} isIdentity />
-                        ) : (
-                          <EmptyDash />
-                        )}
-                      </TableCell>
-                      <TableCell>
-                        {toDid ? (
-                          <AccountOrDidTextField value={toDid} isIdentity />
-                        ) : (
-                          <EmptyDash />
-                        )}
-                      </TableCell>
-                      <TableCell align="right">
-                        <Stack
-                          direction="row"
-                          spacing={1}
-                          justifyContent="flex-end"
-                          alignItems="center"
-                        >
-                          {tx.amount}
-                          {asset.isNftCollection &&
-                            tx.nftIds &&
-                            tx.nftIds.length > 0 && (
-                              <NftTooltip nfts={tx.nftIds} />
-                            )}
-                        </Stack>
-                      </TableCell>
-                      <TableCell align="center">
-                        <TransactionDetailsTooltip
-                          instructionId={tx.instructionId}
-                          instructionMemo={tx.memo}
-                          fundingRound={tx.fundingRound}
-                        />
-                      </TableCell>
-                    </TableRow>
-                  );
-                })
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <PaginationFooter paginationController={paginationController} />
-      </Paper>
-    </Stack>
+                      ) : (
+                        <EmptyDash />
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      {tx.venueId ? (
+                        <GenericLink href={`${ROUTES.Venue}/${tx.venueId}`}>
+                          {tx.venueId}
+                        </GenericLink>
+                      ) : (
+                        <EmptyDash />
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <FormattedDate date={tx.createdBlock.datetime} />
+                    </TableCell>
+                    <TableCell>
+                      <Chip
+                        label={eventInfo.label}
+                        color={eventInfo.color}
+                        size="small"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      {fromDid ? (
+                        <AccountOrDidTextField value={fromDid} isIdentity />
+                      ) : (
+                        <EmptyDash />
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      {toDid ? (
+                        <AccountOrDidTextField value={toDid} isIdentity />
+                      ) : (
+                        <EmptyDash />
+                      )}
+                    </TableCell>
+                    <TableCell align="right">
+                      <Stack
+                        direction="row"
+                        spacing={1}
+                        justifyContent="flex-end"
+                        alignItems="center"
+                      >
+                        {tx.amount}
+                        {asset.isNftCollection &&
+                          tx.nftIds &&
+                          tx.nftIds.length > 0 && (
+                            <NftTooltip nfts={tx.nftIds} />
+                          )}
+                      </Stack>
+                    </TableCell>
+                    <TableCell align="center">
+                      <TransactionDetailsTooltip
+                        instructionId={tx.instructionId}
+                        instructionMemo={tx.memo}
+                        fundingRound={tx.fundingRound}
+                      />
+                    </TableCell>
+                  </TableRow>
+                );
+              })
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <PaginationFooter paginationController={paginationController} />
+    </>
   );
 }
