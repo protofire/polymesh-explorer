dcc60fc77e9ff38fda1b6a2d7e9304776343a513
Gera Barboni <gerardobarboni@gmail.com>
2025-03-12 09:35:29 -0300

    contact modal

--- src/config/images.ts
+++ src/config/images.ts
@@ -1,5 +1,5 @@
 import APP_BACKGROUD from 'public/assets/background.png';
-
+import PROTOFIRE_LOGO from 'public/assets/protofire_logo.svg';
 // Icons image
 export const SPINNER_IMAGE = '/assets/loading_image.png';
 export const SUBSCAN_LOGO = '/assets/subscan_logo.png';
@@ -7,4 +7,4 @@ export const SUBSCAN_LOGO = '/assets/subscan_logo.png';
 // Icons image
 export const FAVICON = '/ico/favicon.ico';
 
-export { APP_BACKGROUD };
+export { APP_BACKGROUD, PROTOFIRE_LOGO };
